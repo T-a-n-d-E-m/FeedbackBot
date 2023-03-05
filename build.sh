34047e84a0fb7e1fe40dec2b2cd3f78aa1884f00
tandem <the_tricky_trickster@protonmail.com>
2023-03-05 21:27:24 +1000

    Add -O3 to build options.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++20 -Wall -Werror -Wpedantic -Wno-unused-function feedbackbot.cpp -ldpp -o feedbackbot
+g++ -std=c++20 -O3 -Wall -Werror -Wpedantic -Wno-unused-function feedbackbot.cpp -ldpp -o feedbackbot
