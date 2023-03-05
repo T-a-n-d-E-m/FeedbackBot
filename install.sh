3513942063cb5197cd7a09828a7c64470986dc3c
tandem <the_tricky_trickster@protonmail.com>
2023-03-05 21:07:30 +1000

    Fixed variable naming error.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ INSTALL_DIR="/opt/FeedbackBot"
 # This script will copy files from the repository to where they need to be and restart the feedbackbot.service.
 # Also needs:
 #     1) A group account: sudo addgroup --system feedbackbot
-#     2) A user account: sudo adduser --system --ingroup feedbackbot --home=$(INSTALL_DIR) --disabled-login feedbackbot
+#     2) A user account: sudo adduser --system --ingroup feedbackbot --home=$INSTALL_DIR --disabled-login feedbackbot
 #     3) A discord.token file in $(INSTALL_DIR)
 
 if [ $(id -u) -ne 0 ]; then
@@ -17,14 +17,14 @@ fi
 
 
 # Install the executable and change permissions and ownership
-cp feedbackbot $(INSTALL_DIR)
-chown feedbackbot:feedbackbot $(INSTALL_DIR)/feedbackbot
-chmod 500 $(INSTALL_DIR)/feedbackbot
+cp feedbackbot $INSTALL_DIR
+chown feedbackbot:feedbackbot $INSTALL_DIR/feedbackbot
+chmod 500 $INSTALL_DIR/feedbackbot
 
 # discord.token - not versioned!
-cp discord.token $(INSTALL_DIR)
-chown feedbackbot:feedbackbot $(INSTALL_DIR)/discord.token
-chmod 400 $(INSTALL_DIR)/discord.token
+cp discord.token $INSTALL_DIR
+chown feedbackbot:feedbackbot $INSTALL_DIR/discord.token
+chmod 400 $INSTALL_DIR/discord.token
 
 cp feedbackbot.service /etc/systemd/system
 chown root:root /etc/systemd/system/feedbackbot.service
